ee1444dfcee0df8406f8c91e7727145ba2c19536
Anup Chenthamarakshan <anupc@dropbox.com>
2016-04-11 08:27:07 -0700

    Move exec handler to its own method.

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func writeError(w http.ResponseWriter, statusCode int, err error) {
 	fmt.Fprintf(w, "%v", err)
 }
 
-func ensureCached(cacheBaseUrl string, file *build_remote.FileEntry, workDir string) error {
+func ensureCached(cacheBaseURL string, file *build_remote.FileEntry, workDir string) error {
 	filePath := filepath.Join(workDir, file.Path)
 	if _, err := os.Stat(filePath); err == nil || !os.IsNotExist(err) {
 		return nil
@@ -32,7 +32,7 @@ func ensureCached(cacheBaseUrl string, file *build_remote.FileEntry, workDir str
 		return err
 	}
 
-	fetchPath := fmt.Sprintf("%s/%s", cacheBaseUrl, file.Path)
+	fetchPath := fmt.Sprintf("%s/%s", cacheBaseURL, file.Path)
 	if resp, err := http.Get(fetchPath); err != nil {
 		return err
 	} else {
@@ -48,41 +48,45 @@ func ensureCached(cacheBaseUrl string, file *build_remote.FileEntry, workDir str
 	return nil
 }
 
-func main() {
-	port := flag.Int("port", 1234, "Port to listen on")
-	cacheBaseUrl := flag.String("cache-base-url", "http://localhost:5701/hazelcast/rest/maps/hazelcast-build-cache", "Base of cache URL to connect to")
-	tmpDirRoot := flag.String("tmp-dir-root", "/tmp/", "Root of temporary directory")
+func HandleBuildRequest(w http.ResponseWriter, r *http.Request) {
+	workReq := new(build_remote.RemoteWorkRequest)
+	err := jsonpb.Unmarshal(r.Body, workReq)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
 
+	tmpDir, err := ioutil.TempDir(*tmpDirRoot, "workdir")
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	log.Println(tmpDir)
+	defer os.RemoveAll(tmpDir)
+
+	for _, inputFile := range workReq.GetInputFiles() {
+		if err := ensureCached(*cacheBaseURL, inputFile, tmpDir); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func main() {
 	flag.Parse()
 
 	log.SetFlags(log.Lmicroseconds)
 
 	listenAddr := fmt.Sprintf(":%d", *port)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		workReq := new(build_remote.RemoteWorkRequest)
-		err := jsonpb.Unmarshal(r.Body, workReq)
-		if err != nil {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
-
-		tmpDir, err := ioutil.TempDir(*tmpDirRoot, "workdir")
-		if err != nil {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
-
-		log.Println(tmpDir)
-		defer os.RemoveAll(tmpDir)
-
-		for _, inputFile := range workReq.GetInputFiles() {
-			if err := ensureCached(*cacheBaseUrl, inputFile, tmpDir); err != nil {
-				log.Println(err)
-			}
-		}
-	})
+	http.HandleFunc("/", HandleBuildRequest)
 
 	err := http.ListenAndServe(listenAddr, nil)
 	log.Fatal(err)
 }
+
+var (
+	port         = flag.Int("port", 1234, "Port to listen on")
+	cacheBaseURL = flag.String("cache-base-url", "http://localhost:5701/hazelcast/rest/maps/hazelcast-build-cache", "Base of cache URL to connect to")
+	tmpDirRoot   = flag.String("tmp-dir-root", "/tmp/", "Root of temporary directory")
+)
